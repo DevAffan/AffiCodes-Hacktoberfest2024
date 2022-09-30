a7fcfd43e75d90e3f81efe150ab0e0be35777b6b
Affan Ali <affanali577@gmail.com>
2022-09-30 11:05:13 +0500

    Switch statement structure

---
+++ C#/Switch.php
@@ -0,0 +1,25 @@
+<?php
+
+//Switch statement / Logic in PHP
+//Akternate of IF statement.
+
+
+switch ($animal ) {
+    case 'cow' :
+        echo "The color is Cow" ;
+        break ;
+    case 'dog' :
+        echo "The color is Dog" ;
+        break ;
+    case 'got' :
+        echo "The color is Got" ;
+        break ;
+    case 'cat' :
+        echo "Color is Cat" ;
+        break ;
+    case 'lion' :
+        echo "Color is Lion" ;
+        break ;
+    default:
+        echo 'Error message.';
+}
