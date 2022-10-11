c339fe820ec56a0b207067db87982766108af533
Affan Ali <affanali577@gmail.com>
2022-10-11 14:10:01 +0500

    Object Attributes to Variables in Javascript demonstration

---
+++ JavaScript/object_attributes_to_variables.js
@@ -0,0 +1,17 @@
+// assigning object attributes to variables
+const person = {
+    name: 'Affan Ali',
+    age: 23,
+    gender: 'Male',
+    country: 'Pakistan',
+}
+
+let name = person.name;
+let age = person.age;
+let gender = person.gender;
+let country = person.country;
+
+console.log('Name: '+name); // Affan Ali
+console.log('Age: '+age); // 23
+console.log('Gender: '+gender); // Male
+console.log('Country: '+country); // Pakistan
