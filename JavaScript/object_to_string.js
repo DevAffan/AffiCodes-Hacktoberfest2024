608d2606ad5c95a56d6c36bc1a666012654f3946
Affan Ali <affanali577@gmail.com>
2022-10-11 11:36:36 +0500

    Converting an object in to string in javascript

---
+++ JavaScript/object_to_string.js
@@ -0,0 +1,16 @@
+//Converting an object to a string
+
+//Object
+const person = {
+    name: 'Affan',
+    age: 23
+}
+
+//coverting object to string
+const result =  JSON.stringify(person);
+
+//print results
+console.log(result);
+
+//checking type
+console.log(typeof result);
