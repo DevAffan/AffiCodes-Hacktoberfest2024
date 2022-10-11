6ecb6165eee93579c323da058bc14f792ffc0273
Affan Ali <affanali577@gmail.com>
2022-10-11 14:02:37 +0500

    Class in Javascript demonstration

---
+++ JavaScript/Data Structure/class_demo.js
@@ -0,0 +1,14 @@
+// Demonstrating the class in JavaScript
+class Person {
+    //constructor
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+// creating an object
+const person1 = new Person('Affan');
+const person2 = new Person('Ali');
+
+console.log(person1.name); // It will print Affan
+console.log(person2.name); // It will print Ali
