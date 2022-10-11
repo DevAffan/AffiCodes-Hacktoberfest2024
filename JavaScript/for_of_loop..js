486f5b4bccc88cd7f23bc76359d9cbc965dd3da9
Affan Ali <affanali577@gmail.com>
2022-10-11 15:06:32 +0500

    For Of loop in javascript

---
+++ JavaScript/for_of_loop..js
@@ -0,0 +1,9 @@
+// array of languages
+const Languages = ['Python', 'Javascript', 'C++', 'PHP'];
+
+// using for...of loop
+for ( let element of Languages ) {
+
+    // display the values
+    console.log(element);
+}
