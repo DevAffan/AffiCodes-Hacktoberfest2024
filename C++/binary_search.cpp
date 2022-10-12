c666f3f49f6d3b7d86fe4f6d825d2de7de1d197b
HARSH DEV <98793412+Harshdev625@users.noreply.github.com>
2022-10-12 16:29:39 +0530

    Update binary_search.cpp

--- C++/binary_search.cpp
+++ C++/binary_search.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 int binarysearch(int *a, int n, int x)
 {
+    // Intialize start as the start point
+    // end as the end of an array
+    // Mid variable to be search in an array.
     int start = 0;
     int end = n - 1;
     int mid;
@@ -25,13 +28,16 @@ int binarysearch(int *a, int n, int x)
 }
 int main()
 {
+    // Input the size of an array.
     int n;
     cin >> n;
+    //created a Dynamic Array.
     int *a = new int[n];
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
     }
+    // Input the number to search in an array.
     int x;
     cin >> x;
     cout << binarysearch(a, n, x);
