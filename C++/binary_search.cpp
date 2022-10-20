35bc93563ad1a7df3da62b2b0dd510cfbd1379e2
wasib hussain <wasibhussain3@gmail.com>
2022-10-20 12:05:24 +0500

    first commit

--- C++/binary_search.cpp
+++ C++/binary_search.cpp
@@ -1,45 +1,30 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int binarysearch(int *a, int n, int x)
-{
-    // Intialize start as the start point
-    // end as the end of an array
-    // Mid variable to be search in an array.
-    int start = 0;
-    int end = n - 1;
-    int mid;
-    while (end >= start)
-    {
-        mid = (start + end) / 2;
-        if (a[mid] == x)
-        {
-            return mid;
-        }
-        else if (a[mid] > x)
-        {
-            end = mid - 1;
-        }
-        else
-        {
-            start = mid + 1;
-        }
-    }
-    return -1;
-}
 int main()
 {
-    // Input the size of an array.
-    int n;
-    cin >> n;
-    //created a Dynamic Array.
-    int *a = new int[n];
-    for (int i = 0; i < n; i++)
+    int A[10] , n = 5 , l = 0 , h = n - 1 , mid , key;
+    cout<<"Enter elements:";
+    for(int i = 0 ; i < n ; i ++)
     {
-        cin >> a[i];
+        cin>>A[i];
     }
-    // Input the number to search in an array.
-    int x;
-    cin >> x;
-    cout << binarysearch(a, n, x);
-    delete[] a;
+    cout<<"Enter key element:";
+    cin>>key;
+    while (l<=h)
+    {
+        mid = (l + h )/2;
+        if(key == A[mid])
+        {
+            cout<<"Found at "<<mid;
+            return 0;
+        }
+        else if(key<A[mid])
+        h = mid - 1;
+        else
+        l = mid + 1;
+    }
+
+     cout<<"Not found";
+    
+    return 0;
 }
