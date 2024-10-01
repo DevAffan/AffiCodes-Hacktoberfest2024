cb72f2fbc811b30fb770d6b44fd92ac6567a4665
Sourabh Singh Rajput <146932359+SourabhRajput123@users.noreply.github.com>
2024-10-01 14:54:41 +0530

    Added Knapsack.java File
    Added 0/1 Knapsack Problem algorithm implementation in Java using dynamic programming.

---
+++ Java/Knapsack.java
@@ -0,0 +1,27 @@
+public class Knapsack {
+
+    public static int knapsack(int[] weights, int[] values, int capacity) {
+        int n = weights.length;
+        int[][] dp = new int[n + 1][capacity + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int w = 0; w <= capacity; w++) {
+                if (weights[i - 1] <= w) {
+                    dp[i][w] = Math.max(dp[i - 1][w], dp[i - 1][w - weights[i - 1]] + values[i - 1]);
+                } else {
+                    dp[i][w] = dp[i - 1][w];
+                }
+            }
+        }
+        return dp[n][capacity];
+    }
+
+    public static void main(String[] args) {
+        int[] weights = {1, 2, 3, 4};
+        int[] values = {10, 20, 30, 40};
+        int capacity = 5;
+
+        int maxValue = knapsack(weights, values, capacity);
+        System.out.println("Maximum value in knapsack: " + maxValue);
+    }
+}
