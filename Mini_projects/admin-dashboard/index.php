e1a4ae45ae21ff3523216bdb648e04ec692ac2ae
DilankaDI <dilshandilanka96@gmail.com>
2024-10-04 11:01:12 +0530

    started mini project Admin Dashboard

---
+++ Mini_projects/admin-dashboard/index.php
@@ -0,0 +1,13 @@
+<?php
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Admin Dashboard</title>
+        <link rel="stylesheet" href="AffiCodes-Hacktoberfest2024/Mini_projects/admin-dashboard/src/css/index.css">
+    </head>
+    <body>
+        <?php include 'navbar.php'; ?>
+    </body>
+</html>
+?>
---
+++ Mini_projects/admin-dashboard/navbar.php
@@ -0,0 +1,18 @@
+<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="AffiCodes-Hacktoberfest2024/Mini_projects/admin-dashboard/src/css/nav.css">
+</head>
+<body>
+  <div class="navbar">
+    <a href="index.php">Dashboard</a>
+    <a href="about.php">Users Count</a>
+    <a href="services.php">About</a>
+    <a href="contact.php">Contact</a>
+  </div>
+</body>
+</html>
+?>
